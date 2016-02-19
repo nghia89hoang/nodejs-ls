501dffd713e31307cd9aa4bb5fcd6cb69ec7ee36
Adam Crabtree <dude@noderiety.com>
2016-02-19 14:59:18 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+require('./helper')
+
+function* ls() {
+  // Use 'yield' in here
+  console.log('Executing ls function...')
+  
+  // Your implementation here
+}
+
+module.exports = ls

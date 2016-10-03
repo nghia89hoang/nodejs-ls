b2652eab7f972c03351a7a47d6ba574c039caa7d
Adam Crabtree <dude@noderiety.com>
2016-10-02 21:58:07 -0700

    Cleanup helper

--- helper.js
+++ helper.js
@@ -1,12 +1,10 @@
-require('trycatch').configure({'long-stack-traces': true})
+require('trycatch').configure({ 'long-stack-traces': true })
 require('songbird')
-
-process.on('uncaughtException', logError)
-
-process.on('uncaughtApplicationException', logError)
-
-process.on('unhandledRejection', logError)
 
 function logError(err) {
   console.log(err.stack)
 }
+
+process.on('uncaughtException', logError)
+process.on('uncaughtApplicationException', logError)
+process.on('unhandledRejection', logError)

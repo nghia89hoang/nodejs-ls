e62efd22739adf7403398525d31acdc87b199db2
Adam Crabtree <dude@noderiety.com>
2016-10-02 19:32:00 -0700

    Fix main & scripts

--- helper.js
+++ helper.js
@@ -1,10 +1,5 @@
-process.nextTick(() => {
-  require('safeguards').noSynchronousIO(require.main)
-})
-
 require('trycatch').configure({'long-stack-traces': true})
 require('songbird')
-require('safeguards')
 
 process.on('uncaughtException', logError)
 
@@ -15,5 +10,3 @@ process.on('unhandledRejection', logError)
 function logError(err) {
   console.log(err.stack)
 }
-
-// TODO: Delete all core non-promise APIs
